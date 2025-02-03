1e4fcdc2dbc6807ee6f463e96912eab094513f38
Sainal <sainalkk6@gmail.com>
2025-02-03 09:35:12 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+import http from "http";
+import { Server } from "socket.io";
+
+const httpServer = http.createServer();
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("connected");
+    socket.on('join_room', (roomId) => {
+        console.log("joined", roomId);
+        socket.join(roomId);
+    });
+
+    socket.on("send_message", (data) => {
+        console.log("sending message has been initialised");
+        io.in(data.roomId).emit('receive_msg', data);
+    });
+
+    socket.on("disconnect", (reason) => {
+        console.log(`Socket has been disconnected ${reason}`);
+    });
+});
+
+const PORT = process.env.PORT || 1100;
+
+httpServer.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
